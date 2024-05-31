5b237082ff19dd80ba8fc7bf18a62ca3254ecfc7
iKenS <64600399+kension1st@users.noreply.github.com>
2024-05-31 18:28:55 +0800

    Update !codingg.py

--- !codingg.py
+++ !codingg.py
@@ -6,57 +6,64 @@ from PyQt5.QtWidgets import (
         QPushButton, QLabel)
 from random import randint, shuffle
 
+
 class Question():
-    def __init__(self, Question, right_answer, wrong1, wrong2, wrong3):
-        self.Question = Question
+    '''contains a question, a correct answer and three incorrect ones'''
+    def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        self.question = question
         self.right_answer = right_answer
         self.wrong1 = wrong1
         self.wrong2 = wrong2
         self.wrong3 = wrong3
+questions_list = [] 
+questions_list.append(
+        Question('Official language of Brazil', 'Brazilian', 'Portuguese', 'English', 'Spanish')
+        )
 
-question_list = []
-question_list.append(
-        Question('Official language of Brazil', 'Brazilian', 'Portuguese', 'English', 'Spanish'))
-question_list.append(
-        Question('Which color does not appear on the American flag?', "blue", 'green', 'yellow', 'black'))
-question_list.append(
-        Question('Yakult national house', "urasa", 'yurta', 'igloo', 'khata'))
+questions_list.append(
+        Question('Which color does not appear on the American flag?', 'Blue', 'Green', 'Red', 'White'))
 
+questions_list.append(
+        Question('Yakut national house', 'Urasa', 'Yurta', 'Igloo', 'Khata'))
 
 app = QApplication([])
 
-button_ok = QPushButton('Reply')
-lb_Question = QLabel('The most difficult question in the world!')
+btn_OK = QPushButton('Reply') # the reply button
+lb_Question = QLabel('The most difficult question in the world!') # question text
 
-Radiogroupbox = QGroupBox('Answer options')
 
-rbtn_1 = QRadioButton("Option 1")
-rbtn_1 = QRadioButton("Option 2")
-rbtn_1 = QRadioButton("Option 3")
-rbtn_1 = QRadioButton("Option 4")
+RadioGroupBox = QGroupBox('Answer options')
 
-Radiogroup = QButtonGroup
-Radiogroup.addButton(rbtn_1)
-Radiogroup.addButton(rbtn_2)
-Radiogroup.addButton(rbtn_3)
-Radiogroup.addButton(rbtn_4)
+rbtn_1 = QRadioButton('Option 1')
+rbtn_2 = QRadioButton('Option 2')
+rbtn_3 = QRadioButton('Option 3')
+rbtn_4 = QRadioButton('Option 4')
 
-layout_ans1 = QVBoxLayout()
-layout_ans2 = QVBoxLayout()
+RadioGroup = QButtonGroup()
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout() # vertical guides inside the horizontal one
 layout_ans3 = QVBoxLayout()
-layout_ans3.addWidget(rbtn_1)
+
+layout_ans3.addWidget(rbtn_1)  # two answer options in the first column
 layout_ans3.addWidget(rbtn_2)
-layout_ans3.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_3) # two answer options in the second column
 layout_ans3.addWidget(rbtn_4)
 
-layout_ans1.addlayout(layout_ans2)
-layout_ans2.addlayout(layout_ans3)
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)  # columns placed in one line
 
-Radiogroupbox.setLayout(layout_ans1)
+RadioGroupBox.setLayout(layout_ans1)
 
-AnsGroupBox = QGroupBox('Test results')
-lb_result = QLabel('Are you right or not?')
-lb_correct = QLabel('Answer will be here!')
+
+AnsGroupBox = QGroupBox("Test results")
+lb_Result = QLabel('are you right or not?') # here it will be written if you are "right" or "wrong"
+lb_Correct = QLabel('answer will be here!') # here will be the text of the correct answer
+
 
 layout_res = QVBoxLayout()
 layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
@@ -66,28 +73,29 @@ layout_line1 = QHBoxLayout() # question
 layout_line2 = QHBoxLayout() # answer options or test result 
 layout_line3 = QHBoxLayout() # "Answer" button
 
+
 layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
-layout_line2.addWidget(Radiogroupbox)
-layout_line2.addWidget(AnsGroupBox)
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
 
-AnsGroupBox.hide()
+AnsGroupBox.hide() # hide the answer panel, the question panel should be visible first 
 layout_line3.addStretch(1)
-layout_line3.addWidget(button_ok, stretch=2)
-
+layout_line3.addWidget(btn_OK, stretch=2) # the button should be large
 layout_line3.addStretch(1)
 
 layout_card = QVBoxLayout()
-layout_card.addlayout(layout_line1, stretch=2)
-layout_card.addlayout(layout_line2, stretch=8)
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
 layout_card.addStretch(1)
-layout_card.addlayout(layout_line3, stretch=1)
+layout_card.addLayout(layout_line3, stretch=1)
 layout_card.addStretch(1)
 layout_card.setSpacing(5)
 
 def show_result():
-    Radiogroupbox.hide()
+    ''' show answer panel '''
+    RadioGroupBox.hide()
     AnsGroupBox.show()
-    button_ok.setText('Next')
+    btn_OK.setText('Next')
 
 def show_question():
     ''' show question panel '''
@@ -104,6 +112,7 @@ def show_question():
 
 answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
 
+
 def ask(q: Question):
     ''' the function writes the values of the question and answers to the corresponding widgets, 
     at the same time the answer options are distributed randomly'''
@@ -114,4 +123,62 @@ def ask(q: Question):
     answers[3].setText(q.wrong3)
     lb_Question.setText(q.question) # question
     lb_Correct.setText(q.right_answer) # reply
-    show_question() # show question panel
+    show_question() # show question panel 
+
+
+def show_correct(res):
+    ''' show the result - set the text passed to the "result" inscription and show the panel we need '''
+    lb_Result.setText(res)
+    show_result()
+
+
+def check_answer():
+    ''' if any answer option is chosen, we need to check and show the answer panel'''
+    if answers[0].isChecked():
+        # right answer!
+        show_correct('Right!')
+        window.score += 1
+        print('Statistics\n-Total questions: ', window.total, '\n-Right answers: ', window.score)
+        print('Rating: ', (window.score/window.total*100), '%')
+    else:
+        if answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+            # wrong answer!
+            show_correct('Wrong answer!')
+            print('Rating: ', (window.score/window.total*100), '%')
+    
+
+
+def next_question():
+    ''' asks a random question from the list '''
+    window.total += 1
+    print('Statistics\n-Total questions: ', window.total, '\n-Right answers: ', window.score)
+    cur_question = randint(0, len(questions_list) - 1)  # we don't need the old value,
+                                                        # this means that you can use a local variable!  
+            # randomly picked a question within the list
+            # if you enter about a hundred words, they will rarely be repeated
+    q = questions_list[cur_question] # picked a question
+    ask(q) # asks
+
+
+def click_OK():
+    ''' determines whether to show another question or check the answer to this one '''
+    if btn_OK.text() == 'Answer':
+        check_answer() # answer check
+    else:
+        next_question() # next question
+
+
+window = QWidget()
+window.setLayout(layout_card)
+window.setWindowTitle('Memo Card')
+
+
+btn_OK.clicked.connect(click_OK) # by clicking on the button, we choose what exactly happens
+
+
+window.score = 0
+window.total = 0
+next_question()
+window.resize(400, 300)
+window.show()
+app.exec()
